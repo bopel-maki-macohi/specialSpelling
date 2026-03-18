4ec62b99e9a7a4792e01b15f48cc0a44f6e45054
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-17 20:02:13 -0500

    map v1

--- source/Global.hx
+++ source/Global.hx
@@ -2,6 +2,6 @@ class Global
 {
 	public static var MAP_POSITION:MapPosition = FRONT_DOOR;
 
-    public static final SPELLINGS_TOTAL:Int = 5;
+    public static final SPELLINGS_TOTAL:Int = 6;
     public static var SPELLINGS_COMPLETED:Int = 0;
 }
