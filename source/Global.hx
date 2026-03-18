4c57a529ba2e8189d291294f56623a377a6177b6
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-17 20:34:30 -0500

    update starting map position

--- source/Global.hx
+++ source/Global.hx
@@ -1,6 +1,6 @@
 class Global
 {
-	public static var MAP_POSITION:MapPosition = FRONT_DOOR;
+	public static var MAP_POSITION:MapPosition = SOUTH_ENTRANCE;
 
     public static final SPELLINGS_TOTAL:Int = 6;
     public static var SPELLINGS_COMPLETED:Int = 0;
